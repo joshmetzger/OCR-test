61677fc2d39d4cd915377f083b6af03d4dd892cf
Josh <josh@josh>
2021-12-28 12:21:24 -0600

    imageMagick seems to now be working in conjuction with teseract

--- upload.php
+++ upload.php
@@ -4,14 +4,23 @@
 
         $file_name = $_FILES['image']['name'];
         $file_tmp = $_FILES['image']['tmp_name'];
+        // $processed_image = null;
 
         move_uploaded_file($file_tmp, 'images/'.$file_name);
         echo "<h3>Image Uploaded Successfully</h3>";
-        echo "<img width='800' height='600' src='images/$file_name'>";
+
+        // imagemagick to process image to something easier for tesseract to read...
+        // shell_exec('convert images/'.$file_name.' -shade 200x50 output.jpg');
+        $img = new Imagick($_SERVER['DOCUMENT_ROOT']."/OCR_test/images/$file_name");
+        $img->scaleImage(2000, 2000, true);
+        $img->writeImage($_SERVER['DOCUMENT_ROOT']."/OCR_test/processed_images/processed_$file_name");
+
+        echo "<img width='800' height='600' src='processed_images/processed_$file_name'>";
+        // echo "<img width='800' height='600' src='images/$file_name'>";
 
         // try shade, other compression, tiff convert, some other image editing, planing if possible...
-        // shell_exec('')
-        shell_exec('/usr/local/bin/tesseract images/'.$file_name.' output');
+        
+        shell_exec('/usr/local/bin/tesseract processed_images/processed_'.$file_name.' output');
 
         echo "<br><h6>Text from proccessed image: </h6>";
 
